7e51b3c8887ba06feae2532872f8ce73fe9af184
Zartab Nakhwa <nzartab@gmail.com>
2021-07-07 21:53:15 +0530

    Core Components

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { 
+import {
   StyleSheet,
-   Text,
-    View ,
-    SafeAreaView,
-    Image,
-    TouchableWithoutFeedback,
-    TouchableHighlight,
-    TouchableOpacity,
-    TouchableNativeFeedback
-  } from 'react-native';
+  Text,
+  Button,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+  Alert
+} from 'react-native';
 
 export default function App() {
 
-  const handlePress=()=>{
+  const handlePress = () => {
     console.log('Text Pressed')
   }
   return (
@@ -27,22 +30,22 @@ export default function App() {
         Now we will make it more longer. Lets see how this get displayed
       </Text>
 
-      <Text 
-        onPress={()=>console.log('Text Pressed from inline function')}
+      <Text
+        onPress={() => console.log('Text Pressed from inline function')}
       >
         Welcome to App Development
       </Text>
 
       <Image source={require("./assets/slack.png")}></Image>
-    <TouchableNativeFeedback onPress={()=>console.log('Image Tapped')}>
-      <Image 
-      source={
-        {
-          uri:"https://www.klikeri.hr/wp-content/uploads/avatar-1-300x300.png",
-          width:300,
-          height:300
-        }
-      }></Image>
+      <TouchableNativeFeedback onPress={() => console.log('Image Tapped')}>
+        <Image
+          source={
+            {
+              uri: "https://www.klikeri.hr/wp-content/uploads/avatar-1-300x300.png",
+              width: 300,
+              height: 300
+            }
+          }></Image>
       </TouchableNativeFeedback>
 
       {/* TouchableWithoutFeedback
@@ -51,17 +54,42 @@ export default function App() {
         TouchableOpacity
       */}
 
+      <Button
+        title="Click Me"
+        onPress={() => {
+          Alert.alert("My Title", "My Message",
+            [{ text: 'Yes', onPress:()=>{console.log('Yes')}},
+            { text: 'No' ,onPress:()=>{console.log('No')}}
+
+            ])
+        }}
+      >
+      </Button>
+
+      <Button 
+        title="Click Input Alert"
+        onPress={() => {
+          Alert.prompt("Select mode","0-CC,1-Cash",(text)=>{
+            console.log(text)
+          })
+        }}
+      >
+      </Button>
+
 
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const containerStyle={backgroundColorr:"dodgerblue"}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:Platform.OS==="android"? 50 : 0
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
