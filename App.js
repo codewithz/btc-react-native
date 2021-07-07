d8b5d7b8e71562944c66713d39eedcd35766a918
Zartab Nakhwa <nzartab@gmail.com>
2021-07-07 20:54:22 +0530

    text,Images,Touchables

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View ,SafeAreaView} from 'react-native';
+import { 
+  StyleSheet,
+   Text,
+    View ,
+    SafeAreaView,
+    Image,
+    TouchableWithoutFeedback,
+    TouchableHighlight,
+    TouchableOpacity,
+    TouchableNativeFeedback
+  } from 'react-native';
 
 export default function App() {
+
+  const handlePress=()=>{
+    console.log('Text Pressed')
+  }
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Text>Hello Boston Technology!!</Text>
+      <Text numberOfLines={1}
+        onPress={handlePress}
+      >
+        Hello React Native.A really long text.
+        Now we will make it more longer. Lets see how this get displayed
+      </Text>
+
+      <Text 
+        onPress={()=>console.log('Text Pressed from inline function')}
+      >
+        Welcome to App Development
+      </Text>
+
+      <Image source={require("./assets/slack.png")}></Image>
+    <TouchableNativeFeedback onPress={()=>console.log('Image Tapped')}>
+      <Image 
+      source={
+        {
+          uri:"https://www.klikeri.hr/wp-content/uploads/avatar-1-300x300.png",
+          width:300,
+          height:300
+        }
+      }></Image>
+      </TouchableNativeFeedback>
+
+      {/* TouchableWithoutFeedback
+        TouchableHighlight,
+        TouchableNativeFeedback,
+        TouchableOpacity
+      */}
+
+
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "tomato",
+    backgroundColor: "white",
     alignItems: 'center',
     justifyContent: 'center',
   },
