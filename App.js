2b5c35c83fb102b52db7f0c5ec20138db777ef7b
Zartab Nakhwa <nzartab@gmail.com>
2021-07-07 19:36:44 +0530

    SafeAreaView

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello Boston Technology!!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "tomato",
     alignItems: 'center',
     justifyContent: 'center',
   },
